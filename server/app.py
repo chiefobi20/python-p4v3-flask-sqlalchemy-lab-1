b8fcd7e07228bbb6bb7cb3740b9d3b77e1cac4c3
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-03-11 16:06:28 -0400

    Completed lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,28 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
 
+
+    if earthquake is None:
+        response_body = {
+            "message": f"Earthquake {id} not found."
+        }
+        return make_response(response_body, 404)
+    else:
+        response_body = earthquake.to_dict()
+        return make_response(response_body, 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    response_body = {
+        "count": len(earthquakes),
+        "quakes": [quake.to_dict() for quake in earthquakes]
+    }
+    return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
